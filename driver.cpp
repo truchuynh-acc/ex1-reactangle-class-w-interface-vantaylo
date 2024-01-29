dbf8e27c62a268e83de9af4b6f3dc65f3edca8e6
vantaylo <vanessalyn.taylor@gmail.com>
2024-01-28 21:55:29 -0600

    testing

--- driver.cpp
+++ driver.cpp
@@ -5,3 +5,41 @@ Project Desc: Abstract Class Public Interface for Abstract Data Types
 Course: COSC-2436-007 (92905) III Data Structures
 Date: Jan 28, 2023
 */
+
+/** @file driver.cpp */
+
+#include <iostream>
+#include "Rectangle.h"
+using namespace std;
+
+int main()
+{
+     Rectangle theRectangle;
+
+     cout << "Intital state of the rectangle" << endl;
+     cout << "The area of the rectangle is " << theRectangle.getArea() << endl
+          << endl;
+
+     theRectangle.set(0.0, 0.0);
+     cout << "Current state of the rectangle" << endl;
+     cout << "The length of the rectangle is " << theRectangle.getLength() << endl;
+     cout << "The width of the rectangle is " << theRectangle.getWidth() << endl;
+     cout << "The area of the rectangle is " << theRectangle.getArea() << endl
+          << endl;
+
+     theRectangle.set(-2, -4);
+     cout << "Current state of the rectangle" << endl;
+     cout << "The length of the rectangle is " << theRectangle.getLength() << endl;
+     cout << "The width of the rectangle is " << theRectangle.getWidth() << endl;
+     cout << "The area of the rectangle is " << theRectangle.getArea() << endl
+          << endl;
+
+     theRectangle.set(2.0, 4.0);
+     cout << "Current state of the rectangle" << endl;
+     cout << "The length of the rectangle is " << theRectangle.getLength() << endl;
+     cout << "The width of the rectangle is " << theRectangle.getWidth() << endl;
+     cout << "The area of the rectangle is " << theRectangle.getArea() << endl
+          << endl;
+
+     return 0;
+}
